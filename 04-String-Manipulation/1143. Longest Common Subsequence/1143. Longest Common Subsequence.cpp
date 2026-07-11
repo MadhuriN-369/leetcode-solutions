f4bc24768af290642256cd598b3bbe760520d590
Madhuri N <231685118+MadhuriN-369@users.noreply.github.com>
2026-07-12 00:42:15 +0530

    [1143] [Time Beats: 65.11%] [Memory Beats: 53.68%] - Leet2Hub

--- 04-String-Manipulation/1143. Longest Common Subsequence/1143. Longest Common Subsequence.cpp
+++ 04-String-Manipulation/1143. Longest Common Subsequence/1143. Longest Common Subsequence.cpp
@@ -15,7 +15,19 @@
 15    }
 16    int longestCommonSubsequence(string text1, string text2) {
 17        int n1 = text1.size(), n2 = text2.size();
-18        vector<vector<int>>dp(n1, vector<int>(n2, -1));
-19        return solve(0, 0, text1, text2, dp);
-20    }
-21};
+18        vector<vector<int>>dp(n1+1, vector<int>(n2+1, 0));
+19
+20        for(int i = n1-1; i>= 0; i--){
+21            for(int j = n2-1; j >= 0; j--) {
+22                int res = 0;
+23                if(text1[i] == text2[j]){
+24                    res += 1 + dp[i+1][j+1];
+25                }
+26                res = max(res, dp[i+1][j]);
+27                res = max(res, dp[i][j+1]);
+28                dp[i][j] = res;
+29            }
+30        }
+31        return dp[0][0];
+32    }
+33};
