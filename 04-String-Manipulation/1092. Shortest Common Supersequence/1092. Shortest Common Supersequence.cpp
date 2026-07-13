47d46bbb74b7e0b065c722a2abf3c0af51d63601
Madhuri N <231685118+MadhuriN-369@users.noreply.github.com>
2026-07-13 11:51:36 +0530

    [1092] [Time Beats: 36.90%] [Memory Beats: 5.94%] - Leet2Hub

---
+++ 04-String-Manipulation/1092. Shortest Common Supersequence/1092. Shortest Common Supersequence.cpp
@@ -0,0 +1,51 @@
+1class Solution {
+2public:
+3    string lcs(string &s1, string &s2) {
+4        int n = s1.size(), m = s2.size();
+5        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+6        for(int i = 1; i <= n; i++) {
+7            for(int j = 1; j <= m; j++) {
+8                if(s1[i-1] == s2[j-1]) {
+9                    dp[i][j] = 1 + dp[i-1][j-1];
+10                }
+11                else{
+12                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+13                }
+14            }
+15        }
+16        //try to rebuild the string supersubsequence #shortest
+17        int i = n, j = m;
+18        string str = ;
+19        while(i > 0 && j > 0) {
+20            if(s1[i-1] == s2[j-1]){
+21                str = str + s1[i-1];
+22                i--;
+23                j--;
+24            }
+25            else{
+26                if(dp[i][j] == dp[i-1][j]){
+27                    str = str + s1[i-1];
+28                    i--;
+29                }
+30                else{
+31                    str = str + s2[j-1];
+32                    j--;
+33                }
+34            }
+35        }
+36        while(i > 0) {
+37            str = str + s1[i-1];
+38            i--;
+39        }
+40        while(j > 0) {
+41            str = str + s2[j-1];
+42            j--;
+43        }
+44        cout<<str;
+45        reverse(str.begin(), str.end());
+46        return str;
+47    }
+48    string shortestCommonSupersequence(string str1, string str2) {
+49        return lcs(str1, str2);
+50    }
+51};
