7ef69247ad7c880a808145d00f1310e1060b04dc
Madhuri N <231685118+MadhuriN-369@users.noreply.github.com>
2026-07-21 00:37:32 +0530

    [72] [Time Beats: 91.05%] [Memory Beats: 42.27%] - Leet2Hub

--- 04-String-Manipulation/72. Edit Distance/72. Edit Distance.cpp
+++ 04-String-Manipulation/72. Edit Distance/72. Edit Distance.cpp
@@ -21,7 +21,29 @@
 21    }
 22    int minDistance(string word1, string word2) {
 23        int n = word1.size(), m = word2.size();
-24        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-25        return solve(0, 0, word1, word2, dp);
-26    }
-27};
+24        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+25        for(int i = 0; i <= n; i++) {
+26            dp[i][m] = n-i;
+27        }
+28        for(int j = 0; j <= m; j++) {
+29            dp[n][j] = m-j;
+30        }
+31
+32        for(int i = n-1; i >= 0; i--) {
+33            for(int j = m-1; j >= 0; j--) {
+34                int res = 1e8;
+35                if(word1[i] == word2[j]) res = min(res, dp[i+1][j+1]);
+36                
+37                //insert
+38                res = min(res, 1 + dp[i][j+1]);
+39                //delete
+40                res = min(res, 1 + dp[i+1][j]);
+41                //replace
+42                if(word1[i] != word2[j]) res = min(res, 1 + dp[i+1][j+1]);
+43                dp[i][j] = res;
+44            }
+45        }
+46
+47        return dp[0][0];
+48    }
+49};
