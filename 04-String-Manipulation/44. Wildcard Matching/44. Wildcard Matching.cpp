bf487977497dc9180fa048467d877a5ee49de0da
Madhuri N <231685118+MadhuriN-369@users.noreply.github.com>
2026-07-24 01:49:31 +0530

    [44] [Time Beats: 37.79%] [Memory Beats: 12.97%] - Leet2Hub

--- 04-String-Manipulation/44. Wildcard Matching/44. Wildcard Matching.cpp
+++ 04-String-Manipulation/44. Wildcard Matching/44. Wildcard Matching.cpp
@@ -1,29 +1,32 @@
 1class Solution {
 2public:
-3    int dp[2001][2001];
-4    bool solve(int i, int j, string &s, string &p) {
-5        if(i == s.size() && j == p.size()) return true;
-6        if(i == s.size()) {
-7            for(; j < p.size(); j++){
-8                if(p[j] != '*') return false;
-9            }
-10            return true;
-11        }
-12        if(j == p.size()) return false;
-13        if(dp[i][j] != -1) return dp[i][j];
-14        if(p[j] == '?') return dp[i][j] = solve(i+1, j+1, s, p);
-15        bool res = false;
-16        if(p[j] == '*') {
-17            res = res | solve(i+1, j, s, p);
-18            res = res | solve(i+1, j+1, s, p);
-19            res = res | solve(i, j+1, s, p);
-20            return dp[i][j] = res;
-21        }
-22        if(s[i] == p[j]) return dp[i][j] = solve(i+1, j+1, s, p);
-23        return dp[i][j] = false;
-24    }
-25    bool isMatch(string s, string p) {
-26        memset(dp, -1, sizeof(dp));
-27        return solve(0, 0, s, p);
-28    }
-29};
+3    bool isMatch(string s, string p) {
+4        int n = s.size(), m = p.size();
+5        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+6        dp[n][m] = 1;
+7        for(int j = m-1; j >= 0; j--){
+8            dp[n][j] = (p[j] == '*') && dp[n][j+1];
+9        }
+10        for(int i = n-1; i >= 0; i--){
+11            for(int j = m-1; j >= 0; j--) {
+12                if(p[j] == '?') {
+13                    dp[i][j] = dp[i+1][j+1];
+14                    continue;
+15                }
+16                bool res = false;
+17                if(p[j] == '*') {
+18                    res = res | dp[i+1][j];
+19                    res = res | dp[i][j+1];
+20                    dp[i][j] = res;
+21                    continue;
+22                }
+23                if(s[i] == p[j]) {
+24                    dp[i][j] = dp[i+1][j+1];
+25                    continue;
+26                }
+27                dp[i][j] = false;
+28            }
+29        }
+30        return dp[0][0];
+31    }
+32};
