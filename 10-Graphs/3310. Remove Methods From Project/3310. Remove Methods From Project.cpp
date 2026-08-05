55ed543941fa21872fd899c1f319ab46b802e909
Madhuri N <231685118+MadhuriN-369@users.noreply.github.com>
2026-08-06 01:03:11 +0530

    [3310] [Time Beats: 67%] [Memory Beats: 11.38%] - Leet2Hub

--- 10-Graphs/3310. Remove Methods From Project/3310. Remove Methods From Project.cpp
+++ 10-Graphs/3310. Remove Methods From Project/3310. Remove Methods From Project.cpp
@@ -1,55 +1,49 @@
-1constexpr int MAXN = 100005;
-2
-3class Solution {
-4public:
-5    vector<int> remainingMethods(int n, int k,
-6                                 vector<vector<int>>& invocations) {
-7        vector<vector<int>> edges(n);
-8        vector<int> inDegree(n, 0);
-9
-10        bitset<MAXN> suspicious;
-11
-12        for (const auto& inv : invocations) {
-13            edges[inv[0]].push_back(inv[1]);
-14            inDegree[inv[1]]++;
-15        }
-16
-17        queue<int> q;
-18        q.push(k);
-19
-20        suspicious.set(k);
-21
-22        while (!q.empty()) {
-23            int u = q.front();
-24            q.pop();
-25            for (int v : edges[u]) {
-26                inDegree[v]--;
-27
-28                if (!suspicious.test(v)) {
-29                    q.push(v);
-30                    suspicious.set(v);
-31                }
-32            }
-33        }
-34
-35        bool canRemoveAll = true;
-36        vector<int> remaining;
-37
-38        for (int i = 0; i < n; i++) {
-39            if (suspicious.test(i) && inDegree[i] > 0) {
-40                canRemoveAll = false;
-41                break;
-42            } else if (!suspicious.test(i)) {
-43                remaining.push_back(i);
-44            }
+1class Solution {
+2public:
+3    void dfs(int node, vector<int>&vis, vector<vector<int>>&adj){
+4        if(vis[node]) return;
+5        vis[node] = 1;
+6        for(auto it: adj[node]){
+7            if(!vis[it]) dfs(it, vis, adj);
+8        }
+9    }
+10    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+11        if(invocations.size() == 0){
+12            vector<int>ans;
+13            for(int i = 0; i < n; i++){
+14                if(i != k) ans.push_back(i);
+15            }
+16            return ans;
+17        }
+18        vector<vector<int>>adj(n);
+19        for(auto it:invocations){
+20            int u = it[0];
+21            int v = it[1];
+22            adj[u].push_back(v);
+23        }
+24        vector<int>vis(n, 0);
+25        dfs(k, vis, adj);
+26        vector<int>ans;
+27        unordered_set<int>st;
+28        for(auto it:invocations){
+29            int u = it[0];
+30            int v = it[1];
+31            if(vis[v] == 1 && vis[u] != 1){
+32                ans.clear();
+33                for(int i = 0; i < n; i++) ans.push_back(i);
+34                return ans;
+35            }
+36            if(vis[u] == 0 && vis[v] == 0){
+37                if(st.find(u) == st.end()) ans.push_back(u);
+38                if(st.find(v) == st.end()) ans.push_back(v);
+39                st.insert(u);
+40                st.insert(v);
+41            }
+42        }
+43        for(int i = 0; i < n; i++){
+44            if(st.find(i) == st.end() && !vis[i]) ans.push_back(i);
 45        }
-46
-47        if (!canRemoveAll) {
-48            vector<int> allNodes(n);
-49            iota(allNodes.begin(), allNodes.end(), 0);
-50            return allNodes;
-51        }
-52
-53        return remaining;
-54    }
-55};
+46        sort(ans.begin(), ans.end());
+47        return ans;
+48    }
+49};
