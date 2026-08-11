4f309a4651475ad5c550fd3a2d0a7ebbff9f7af1
Madhuri N <231685118+MadhuriN-369@users.noreply.github.com>
2026-08-11 21:03:58 +0530

    [141] [Time Beats: 83.56%] [Memory Beats: 23.46%] - Leet2Hub

--- 01-Arrays-and-Hashing/141. Linked List Cycle/141. Linked List Cycle.cpp
+++ 01-Arrays-and-Hashing/141. Linked List Cycle/141. Linked List Cycle.cpp
@@ -9,15 +9,12 @@
 9class Solution {
 10public:
 11    bool hasCycle(ListNode *head) {
-12        unordered_set<ListNode*>st;
-13        ListNode*temp = head;
-14        while(temp != nullptr) {
-15            if(st.find(temp) != st.end()){
-16                return true;
-17            }
-18            st.insert(temp);
-19            temp = temp->next;
-20        }
-21        return false;
-22    }
-23};
+12        ListNode* slow = head, *fast = head;
+13        while(fast != nullptr && fast->next != nullptr){
+14            fast = fast->next->next;
+15            slow = slow->next;
+16            if(fast == slow) return true;
+17        }
+18        return false;
+19    }
+20};
