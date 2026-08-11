7b5bb47c3117ded75f646951b2daa894e4a730a3
Madhuri N <231685118+MadhuriN-369@users.noreply.github.com>
2026-08-11 21:35:40 +0530

    [142] [Time Beats: 45.25%] [Memory Beats: 86.67%] - Leet2Hub

--- 01-Arrays-and-Hashing/142. Linked List Cycle II/142. Linked List Cycle II.cpp
+++ 01-Arrays-and-Hashing/142. Linked List Cycle II/142. Linked List Cycle II.cpp
@@ -1,1 +1,28 @@
- *     int val; *     ListNode *next; *     ListNode(int x) : val(x), next(NULL) {} * }; */class Solution {public:    ListNode *detectCycle(ListNode *head) {        ListNode* temp = head;        while(temp != nullptr){            if(st.find(temp) != st.end()){        unordered_set<ListNode*>st;                return temp;            }            st.insert(temp);            temp = temp->next;        }        return nullptr;    }};
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode(int x) : val(x), next(NULL) {}
+7 * };
+8 */
+9class Solution {
+10public:
+11    ListNode *detectCycle(ListNode *head) {
+12        if(head == nullptr || head->next == nullptr) return nullptr;
+13        if(head->next->next == head) return head;
+14        ListNode* slow = head, *fast = head;
+15        while(fast != nullptr && fast->next != nullptr) {
+16            fast = fast->next->next;
+17            slow = slow->next;
+18            if(fast == slow) break;
+19        }
+20        if(fast == nullptr || fast->next == nullptr) return nullptr;
+21        slow = head;
+22        while(slow != fast){
+23            slow = slow->next;
+24            fast = fast->next;
+25        }
+26        return slow;
+27    }
+28};
